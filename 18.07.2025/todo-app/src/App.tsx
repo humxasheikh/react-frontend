da68e04441d7b7008d13420ba8c411c847bd53ea
Hamza Arshad <hamza.arshad1008@gmail.com>
2025-07-19 17:20:07 +0200

    added the to do app

--- 18.07.2025/todo-app/src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="flex items-center justify-center h-screen bg-black-100">
-      <h1 className="text-4xl font-bold text-blue-600">Tailwind is Working!</h1>
-    </div>
-  );
-}
-
-export default App;
