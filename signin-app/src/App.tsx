f4f59896fc42539c542a8215e88caa9276420601
Hamza Arshad <hamza.arshad1008@gmail.com>
2025-07-20 12:28:05 +0200

    added the basic configuration

---
+++ signin-app/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center bg-gray-300 w-full">
+      <h1 className="text-2xl font-bold bg-white">hellow world</h1>
+    </div>
+  );
+}
+
+export default App;
