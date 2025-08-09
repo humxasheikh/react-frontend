0deb4e689ecb065ee8c735960ff03929ad201225
Hamza Arshad <hamza.arshad1008@gmail.com>
2025-08-09 16:13:06 +0200

    addded the project for the online store

---
+++ ecommerce-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-amber-400 ">
+        <h3 className="w-full  h-full text-amber-950 text-2xl justify-center items-center ">
+          Tailwind is working perfectly
+        </h3>
+      </div>
+    </>
+  );
+}
+
+export default App;
